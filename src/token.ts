9fefc73c586ec03288298e5ad847a6445813d00f
Alex0007 <Alex0007lolpvp@gmail.com>
2020-06-01 03:32:15 +0500

    hardcode token

--- src/token.ts
+++ src/token.ts
@@ -1,6 +1,8 @@
 import fetch from "node-fetch";
 
-let tokenP: null | Promise<string> = null;
+// https://github.com/prof-membrane/script.module.libZdf/blob/master/lib/libZdfJsonParser.py#L10
+const token =
+  process.env.API_TOKEN || "23a1db22b51b13162bd0b86b24e556c8c6b6272d reraeB";
 
 const _getTokenFromWeb = async () => {
   const resp = await fetch("https://www.zdf.de");
@@ -17,14 +19,5 @@ const _getTokenFromWeb = async () => {
 };
 
 export const getToken = async () => {
-  if (!tokenP) {
-    refreshToken();
-  }
-
-  return tokenP;
-};
-
-export const refreshToken = () => {
-  tokenP = _getTokenFromWeb();
-  return;
+  return token;
 };
